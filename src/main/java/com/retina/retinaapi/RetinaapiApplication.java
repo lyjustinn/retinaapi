674bf77b512c748c69b80412a7f0fc11911107de
lyjustnn <ly.justin25@gmail.com>
2021-07-07 23:13:28 -0400

    Initial commit

---
+++ src/main/java/com/retina/retinaapi/RetinaapiApplication.java
@@ -0,0 +1,13 @@
+package com.retina.retinaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetinaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetinaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retina/retinaapi/RetinaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retina.retinaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetinaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
