664083f4824b53f2800c33a8dd47be92309e9509
lyjustnn <ly.justin25@gmail.com>
2021-07-10 15:10:32 -0400

    resolve userDto deserialization error on post request

--- src/main/java/com/retina/retinaapi/mapper/UserDto.java
+++ src/main/java/com/retina/retinaapi/mapper/UserDto.java
@@ -2,11 +2,22 @@ package com.retina.retinaapi.mapper;
 
 public class UserDto {
 
+    private Long id;
+
     private String username;
 
     private String password;
 
+    public UserDto() {
+    }
+
     public UserDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public UserDto(Long id, String username, String password) {
+        this.id = id;
         this.username = username;
         this.password = password;
     }
@@ -25,5 +36,13 @@ public class UserDto {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
